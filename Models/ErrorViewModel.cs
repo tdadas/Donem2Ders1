b5b858bb0090648a4c696ad2343f9ef07d488d67
tdadas <mtdadasoglu@gmail.com>
2025-03-01 17:47:13 +0300

    Donem2Ders1

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace _2._Dönem_1.Ders.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
